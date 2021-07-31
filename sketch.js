1507f8284b0c4025f3a8bcf2b1e9bbebf0c15376
Blue-Eared-Husky <81889254+Blue-Eared-Husky@users.noreply.github.com>
2021-07-30 20:25:17 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var zombieleft,zombieright,bgImg;
+var ground,wall1,wall2,bridge,joinPoint1,joinPoint2,link,stones;
+
+function preload(){
+  zombieleft = loadAnimation('./assets/zombies/zombie1.png.png','./assets/zombies/zombie2.png.png','./assets/zombies/zombie3.png.png','./assets/zombies/zombie4.png.png','./assets/zombies/zombie5.png.png','./assets/zombies/zombie6.png.png','./assets/zombies/zombie7.png.png','./assets/zombies/zombie8.png.png','./assets/zombies/zombie9.png.png','./assets/zombies/zombie10.png.png','./assets/zombies/zombie11.png.png','./assets/zombies/zombie12.png.png');
+  bgImg = loadImage("./assets/background.png");
+}
+
+function setup() {
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  zombie = createSprite(width/2,height-110);
+  zombie.addAnimation("left",zombieleft);
+  zombie.scale = 0.3;
+  zombie.velocityX = 10;
+
+  breakButton = createButton("");
+  breakButton.position(width-200,height/2 - 50);
+  breakButton.class("breakbutton");
+  breakButton.mousePressed(handleButtonPress);
+
+  ground = new Base(width/2,height,width,20);
+  wall1 = new Base(0,height/2,20,height);
+  wall2 = new Base(width,height/2,20,height);
+  bridge = new Bridge(21,{x: 10, y: height-450});
+
+  //Matter.Composite.add(bridge.body,joinPoint1);
+  joinPoint1 = new Base(25,height-450,200,200);
+  joinPoint2 = new Base(width-25,height-450,200,200);
+  link = new Link(bridge,joinPoint2);
+
+  stones = [];
+
+  for (var i = 0; i < 8; i++){
+    var x = random(width/2 - 180, width/2 + 180);
+    var y = random(0, 50);
+    var stone = new Stone(x,y,80,80);
+    stones.push(stone);
+  }
+}
+
+function draw() {
+  background(bgImg);
+  Engine.update(engine);
+
+  bridge.show();
+
+  for (var k = 0; k < stones.length; k++){
+    stones[k].show();
+  }
+
+  if (zombie.position.x < 230){
+    zombie.velocityX = 10;
+    zombie.mirrorX(1);
+  }
+  if (zombie.position.x > width-230){
+    zombie.velocityX = -10;
+    zombie.mirrorX(-1);
+  }
+
+  drawSprites();
+}
+
+function handleButtonPress(){
+  link.dettach();
+  setTimeout(() => {
+    bridge.break();
+  },1500);
+}
